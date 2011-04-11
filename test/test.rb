d4cee3b1e39ff802bba543ebb9def35326bbc792
Manabu Ori <manabu.ori+github@gmail.com>
2011-04-11 22:00:44 +0900

    fix typo.

--- test/test.rb
+++ test/test.rb
@@ -6,6 +6,6 @@ require 'sysstat/sar'
 sar = Sysstat::LinuxSar.new
 sar.parse(ARGV.shift)
 #sar.dump
-sar.exclude_filter = /intr_cpu|intr_xall.\d|net_e?dev\.(lo|sit|usb)/
+sar.exclude_filter = /intr_cpu|intr_xall\.\d|net_e?dev\.(lo|sit|usb)/
 sar.print_csv_header
 sar.print_csv_data
