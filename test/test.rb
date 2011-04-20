798c7df7f12e0c17719de5e23033d76b9f20d9fe
Manabu Ori <manabu.ori+github@gmail.com>
2011-04-21 08:27:28 +0900

    temporary commit

--- test/test.rb
+++ test/test.rb
@@ -1,11 +1,45 @@
 #!/usr/bin/ruby
-# vi: set ts=4 sw=4 et sts=4:
+# -*- coding: utf-8; ruby-indent-level: 4 -*- vi: set ts=4 sw=4 et sts=4:
 
+require 'test/unit'
 require 'sysstat/sar'
 
-sar = Sysstat::LinuxSar.new
-sar.parse(ARGV.shift)
-#sar.dump
-sar.exclude_filter = /intr_cpu|intr_xall\.\d|net_e?dev\.(lo|sit|usb)/
-sar.print_csv_header
-sar.print_csv_data
+class SarMetricTest < Test::Unit::TestCase
+    def setup
+#         puts '==> test start'
+        @obj = Sysstat::SarMetric.new(
+               	'proc/s',
+                'proc',
+                '(-c) process creation activity',
+                0
+               )
+        @input = Hash.new
+        @input[:rhel5] = %q{
+17:00:59       proc/s
+17:01:09        11.89
+17:01:19         0.00
+17:01:29         0.00
+17:01:39         0.00
+17:01:49         0.00
+17:01:59         3.10
+}
+    end
+
+    def teardown
+#         puts '==> test end'
+    end
+
+    def test_name
+        puts "test_name"
+        assert_equal('proc', @obj.name)
+    end
+
+    def test_data
+        puts "test_data"
+        @input[:rhel5].each_line do |line|
+            puts line
+        end
+#         print @input[:rhel5], "\n"
+    end
+end
+
